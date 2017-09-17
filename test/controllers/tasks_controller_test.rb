66aeea864f392a7a85d034c8da68d80c5d3e1beb
Imam Baihaqi <imam.baihaqi1999@gmail.com>
2017-09-17 03:37:11 +0000

    integration tests

--- test/controllers/tasks_controller_test.rb
+++ test/controllers/tasks_controller_test.rb
@@ -1,9 +1,16 @@
 require 'test_helper'
 
 class TasksControllerTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
+  
   test "should get show" do
     get course_task_url(courses(:one), tasks(:one))
     assert_response :redirect
   end
 
+  test "should be able to access preview task" do
+    sign_in(users(:user_one))
+    get course_task_url(courses(:two), tasks(:two))
+    assert_response :success
+  end
 end
