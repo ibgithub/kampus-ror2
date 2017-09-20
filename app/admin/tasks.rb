1111f9ca95cb33373c2e279ecf892aa0e046c6c5
Imam Baihaqi <imam.baihaqi1999@gmail.com>
2017-09-20 21:50:25 +0000

    add ajax for review form

--- app/admin/tasks.rb
+++ app/admin/tasks.rb
@@ -1,5 +1,5 @@
 ActiveAdmin.register Task do
-  permit_params :course_id, :title, :description, :video_url, :image, :preview
+  permit_params :course_id, :title, :description, :video_url, :image, :preview, :position
 
   controller do
     def find_resource
@@ -7,4 +7,37 @@ ActiveAdmin.register Task do
     end
   end
   
+  member_action :sort, method: :post do
+    resource.set_list_position(params[:position])
+  end
+  
+  member_action :move_to_top, method: :post do
+    resource.move_to_top
+    redirect_to admin_course_path(resource.course), notice: "#{resource.title} task moved to top."
+  end
+  
+  show do
+    attributes_table do
+      row :title
+      row :description do
+        raw resource.description
+      end
+      row :video_url
+      row :image
+      row :preview
+      row :position
+    end
+  end
+  
+  form do |f|
+    f.inputs do
+      f.input :title
+      f.input :description, as: :html_editor
+      f.input :video_url
+      f.input :image, hint: resource.image.present? ? image_tag(resource.image.url, height: 300) : content_tag(:span, 'No image')
+      f.input :preview
+      f.input :position
+    end
+    f.actions
+  end
 end
